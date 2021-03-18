44f0f1845215355f03c62374d32fbc2d5c9aa495
Darrell Lawrence <lawrencedt1@gmail.com>
2021-03-18 12:00:13 -0500

    Beginning Javascript logic

---
+++ script/calculator.js
@@ -0,0 +1,35 @@
+function updateDisplay(newValue){
+    var d = document.getElementById('display');
+    
+    switch (newValue){
+        case '':
+            d.innerHTML = '';
+            break;
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            d.innerHTML += newValue;
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            if (d.innerHTML.endsWith('+')){ // if +,-,*, or /, then do nothing
+                break;     
+            }
+            else{
+                d.innerHTML += newValue;
+                break;
+            }
+        case '=':
+            //combine
+            break;
+        }
+    }
